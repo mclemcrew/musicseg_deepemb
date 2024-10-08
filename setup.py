c823c9550d80675e87bf69341c91f82636b53088
Michael Clemens <michael.clemens@outlook.com>
2024-10-08 08:44:13 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,24 +19,24 @@ setup(
    keywords='Music segmentation deep embeddings section fusion',
    license='See LICENSE.txt',
    install_requires=[
-      'torch==1.8.0',
-      'torchvision==0.2.1',
-      'torchsummary==1.5.1',
-      'numpy==1.19.2',
-      'scipy==1.4.1',
-      'scikit-learn==0.22.1',
-      'audioread==2.1.8',
-      'numba==0.48.0',
-      'pandas==1.0.3',
-      'resampy==0.2.2',
-      'SoundFile==0.10.3.post1',
-      'sox==1.3.7',
-      'h5py==3.1.0',
-      'librosa==0.7.0',
-      'tensorflow==2.6.0',
-      'keras==2.6.0',
-      'mir_eval==0.6',
-      'madmom==0.16.1',
-      'matplotlib'
-   ],
+    'torch==1.8.0',
+    'torchvision==0.9.1',  # Updated
+    'torchsummary==1.5.1',
+    'numpy==1.21.5',     # Updated
+    'scipy==1.4.1',
+    'scikit-learn==0.22.1',
+    'audioread==2.1.8',
+    'numba==0.48.0',
+    'pandas==1.0.3',
+    'resampy==0.2.2',
+    'SoundFile==0.10.3.post1',
+    'sox==1.3.7',
+    'h5py==3.1.0',
+    'librosa==0.7.0',
+    'tensorflow>=2.8',   # Updated (consider a more specific version)
+    'keras==2.8',        # Updated (or use the one bundled with TensorFlow)
+    'mir_eval==0.6',
+    'madmom==0.16.1',
+    'matplotlib'
+   ]
 )
